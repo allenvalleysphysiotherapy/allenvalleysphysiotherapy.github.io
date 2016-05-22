c0aa7e0863a07c5317998033637b61510909b7bf
Stu Charlton <stu.charlton@cxpartners.co.uk>
2016-05-22 21:30:58 +0100

    added BrowserSync

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,37 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'inc/css/screen.css' : 'inc/scss/screen.scss'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'inc/css/screen.css' : 'inc/scss/screen.scss'
+        }
+      }
+    },
+    browserSync: {
+      bsFiles: {
+        src : [
+          'inc/css/*.css',
+          'index.html'
+        ],        
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "./"
+        }
+      },
+
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.registerTask('default', ['browserSync', 'watch']);
 }
